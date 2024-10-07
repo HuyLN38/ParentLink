efa7c26ba59028450edac17c3d9eb5d74839b6a1
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-10-07 20:53:51 +0700

    test boxmap

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:parent_link/model/control/control_child_location.dart';
 import 'package:parent_link/model/control/control_child_state.dart';
 import 'package:parent_link/routes/routes.dart';
@@ -28,3 +29,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   // Pass your access token to MapboxOptions so you can load a map
+//   String ACCESS_TOKEN = 'sk.eyJ1IjoiZ2lhbmdndW90MyIsImEiOiJjbTF5b3JiMG4wMGZiMmpzamdqNzJiYmE1In0.R7n0GvDhNY7XXdXDz5S2qA';
+//   MapboxOptions.setAccessToken(ACCESS_TOKEN);
+//
+//   // Define options for your camera
+//   CameraOptions camera = CameraOptions(
+//       center: Point(coordinates: Position(-98.0, 39.5)),
+//       zoom: 2,
+//       bearing: 0,
+//       pitch: 0);
+//
+//   // Run your application, passing your CameraOptions to the MapWidget
+//   runApp(MaterialApp(home: MapWidget(
+//     cameraOptions: camera,
+//   )));
+// }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+// String ACCESS_TOKEN = "sk.eyJ1IjoiZ2lhbmdndW90MyIsImEiOiJjbTF5b3JiMG4wMGZiMmpzamdqNzJiYmE1In0.R7n0GvDhNY7XXdXDz5S2qA";
 
 class SchedulePage extends StatelessWidget {
-  const SchedulePage({super.key});
 
+  const SchedulePage({super.key});
   @override
   Widget build(BuildContext context) {
+    String ACCESS_TOKEN = "sk.eyJ1IjoiZ2lhbmdndW90MyIsImEiOiJjbTF5b3JiMG4wMGZiMmpzamdqNzJiYmE1In0.R7n0GvDhNY7XXdXDz5S2qA";
+    MapboxOptions.setAccessToken(ACCESS_TOKEN);
+    CameraOptions camera = CameraOptions(
+      center: Point(coordinates: Position(-98.0, 39.5)),
+      zoom: 2,
+      bearing: 0,
+      pitch: 0,
+    );
+
+
     return Scaffold(
-      body: const Center(
-        child: Text("Map Page"),
-      ),
+      body: Center(
+        child: MapWidget(
+          cameraOptions: camera,
+        )),
     );
   }
 }
